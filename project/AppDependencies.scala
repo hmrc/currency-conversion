1dbdc4790d5104c4b7043b9c862bf1e601976b96
subhajitchowdhury <subhajit.chowdhury@digital.hmrc.gov.uk>
2018-08-16 13:09:56 +0100

    added it in scope in dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,12 +10,12 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-25" % "1.6.0"
   )
 
-  def test(scope: String = "test") = Seq(
+  def test(scope: String = "test,it") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.mockito" % "mockito-all" % "2.0.2-beta" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test,it",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
 
