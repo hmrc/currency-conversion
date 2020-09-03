69e27d7b91c76563f48599754c8388aacc060821
shreyddas <68951646+shreyddas@users.noreply.github.com>
2020-09-03 17:01:33 +0100

    PAS-835: Dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.46.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.14.0"
   )
 
   def test(scope: String = "test,it") = Seq(
